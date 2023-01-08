4e09e609886edc9b140f3bdc5edc2f74325d541d
Melita <melita6223@gmail.com>
2023-01-08 11:51:57 -0800

    Final Weather app

--- src/index.js
+++ src/index.js
@@ -20,6 +20,55 @@ function date(time) {
   let day = days[date.getDay()];
   return `${day}  ${hours}:${mint}`;
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDey, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` <div class="col-4 border rounded-2">
+        <div class="weather-forecast-date">${formatDay(forecastDey.time)}</div>
+        <img
+          src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+            forecastDey.condition.icon
+          }.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDey.temperature.maximum
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDey.temperature.minimum
+          )} °</span>
+        </div>
+      </div>
+       `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "b08da173443fa6f2c4et5o305194497a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${coordinates.latitude}&lon=${coordinates.longitude}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemp(response) {
   let humi = document.querySelector("#humidity");
   let temper = document.querySelector("#degree");
@@ -45,6 +94,7 @@ function displayTemp(response) {
     "src",
     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${icon}.png`
   );
+  getForecast(response.data.coordinates);
 }
 
 function search(city) {
